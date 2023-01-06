349ac38ede4f6943a3300b34c8838feb0939424f
Agajan Torayev <torayeff@gmail.com>
2023-01-06 10:55:12 +0000

    environment with production statuse

--- mfgrl/envs/mfgenv.py
+++ mfgrl/envs/mfgenv.py
@@ -16,10 +16,13 @@ class MfgEnv(gym.Env):
         ).T  # in $
         self.running_costs = np.array([[10.0, 15.0, 20.0, 5.0, 25.0]]).T  # in $
         self.production_rates = np.array([[1, 1.5, 2, 0.25, 5]]).T  # per unit time
+        self.setup_times = np.array([[5, 7.5, 9, 3.5, 10]]).T  # in time units
 
         self.observation_space = gym.spaces.Dict(
             {
-                "cfgs_mask": gym.spaces.MultiBinary((self.num_cfgs, self.cfgs_limit)),
+                "cfgs_status": gym.spaces.Box(
+                    low=0.0, high=1.0, shape=(self.num_cfgs, self.cfgs_limit)
+                ),
                 "produced": gym.spaces.Box(
                     low=0.0, high=10e6, shape=(self.num_cfgs, self.cfgs_limit)
                 ),
@@ -32,7 +35,7 @@ class MfgEnv(gym.Env):
 
     def _reset_initial_obs(self):
         self.obs = {
-            "cfgs_mask": np.zeros((self.num_cfgs, self.cfgs_limit), dtype=np.float32),
+            "cfgs_status": np.zeros((self.num_cfgs, self.cfgs_limit), dtype=np.float32),
             "produced": np.zeros((self.num_cfgs, self.cfgs_limit), dtype=np.float32),
         }
 
@@ -48,16 +51,37 @@ class MfgEnv(gym.Env):
 
     def step(self, action):
         if action == self.num_cfgs:
+
             # produce products
-            self.obs["produced"] += self.obs["cfgs_mask"] * self.production_rates
+            # .astype(int) ensures that only ready machines contribute
+            self.obs["produced"] += (
+                self.obs["cfgs_status"].astype(int) * self.production_rates
+            )
+            reward = -1.0 * np.sum(
+                self.obs["cfgs_status"].astype(int) * self.running_costs
+            )
+
+            # update cfgs status
+            # add setup_times if 0 < cfg_status <= 1
+            # add small eps to deal with 0.999999xxx
+            # clip [0, 1]
+            updates = np.ceil(self.obs["cfgs_status"])
+            progress = 1 / self.setup_times + 1e-9
+            self.obs["cfgs_status"] = np.clip(
+                self.obs["cfgs_status"] + updates * progress, a_min=0, a_max=1
+            )
+
             self._time_step += 1
-            reward = -1.0 * np.sum(self.obs["cfgs_mask"] * self.running_costs)
+
         else:
             # buy new configuration and do not update time
             # find index with zero value
-            idxs = np.where(self.obs["cfgs_mask"][action] == 0)[0]
+            idxs = np.where(self.obs["cfgs_status"][action] == 0)[0]
             if len(idxs) != 0:
-                self.obs["cfgs_mask"][action][idxs[0]] = 1
+                # initiate the installation of the configuration
+                self.obs["cfgs_status"][action][idxs[0]] = (
+                    1 / self.setup_times[action][0]
+                )
 
             reward = -1.0 * self.purchase_costs[action][0]
 
