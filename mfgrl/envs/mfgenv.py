02acb40cda04115c4b884f5d6eaa77246b2cdcbe
Agajan Torayev <torayeff@gmail.com>
2023-01-07 13:10:22 +0000

    more or less ready environment

--- mfgrl/envs/mfgenv.py
+++ mfgrl/envs/mfgenv.py
@@ -23,45 +23,9 @@ class MfgEnv(gym.Env):
         self.num_cfgs = len(self.market_incurring_costs)
 
         # observation and action spaces
-        # TODO: encode/decode observation!!!
-        self.observation_space = gym.spaces.Dict(
-            {
-                # demand data
-                "demand": gym.spaces.Discrete(self.MAX_PRODUCTS),
-                "demand_time": gym.spaces.Discrete(self.MAX_TIME),
-                # available resources
-                "incurred_costs": gym.spaces.Box(
-                    low=0.0, high=10e9, shape=(self.buffer_size,)
-                ),
-                "recurring_costs": gym.spaces.Box(
-                    low=0.0, high=10e9, shape=(self.buffer_size,)
-                ),
-                "production_rates": gym.spaces.Box(
-                    low=0.0, high=10e9, shape=(self.buffer_size,)
-                ),
-                "setup_times": gym.spaces.Box(
-                    low=0.0, high=10e9, shape=(self.buffer_size,)
-                ),
-                "cfgs_status": gym.spaces.Box(
-                    low=0.0, high=1.0, shape=(self.buffer_size,)
-                ),
-                "produced_counts": gym.spaces.Box(
-                    low=0.0, high=10e9, shape=(self.buffer_size,)
-                ),
-                # market data, changes dynamically
-                "market_incurring_costs": gym.spaces.Box(
-                    low=0.0, high=10e9, shape=(self.num_cfgs,)
-                ),
-                "market_recurring_costs": gym.spaces.Box(
-                    low=0.0, high=10e9, shape=(self.num_cfgs,)
-                ),
-                "market_production_rates": gym.spaces.Box(
-                    low=0.0, high=10e9, shape=(self.num_cfgs,)
-                ),
-                "market_setup_times": gym.spaces.Box(
-                    low=0.0, high=10e9, shape=(self.num_cfgs,)
-                ),
-            }
+        obs_dim = 2 + self.buffer_size * 6 + self.num_cfgs * 4
+        self.observation_space = gym.spaces.Box(
+            low=-np.inf, high=np.inf, shape=(obs_dim,)
         )
         self.action_space = gym.spaces.Discrete(self.num_cfgs + 1)
 
@@ -71,8 +35,8 @@ class MfgEnv(gym.Env):
         # reset buffer
         self.buffer_idx = 0
 
-        # reset observation
-        self.obs = {
+        # reset environment state
+        self._env_state = {
             # demand data
             "demand": self.demand,
             "demand_time": self.demand_time,
@@ -90,7 +54,7 @@ class MfgEnv(gym.Env):
             "market_setup_times": self.market_setup_times,
         }
 
-        return self.obs, self._get_info()
+        return self._get_obs(), self._get_info()
 
     def step(self, action):
         if (0 <= action < self.num_cfgs) and (self.buffer_idx < self.buffer_size):
@@ -98,40 +62,40 @@ class MfgEnv(gym.Env):
         else:
             reward = self.continue_production()
 
-        terminated = self.obs["demand"] <= 0
-        truncated = self.obs["demand_time"] <= 0
+        terminated = self._env_state["demand"] <= 0
+        truncated = self._env_state["demand_time"] <= 0
 
         if truncated:
             reward = -10e6
 
-        return self.obs, reward, terminated, truncated, self._get_info()
+        return self._get_obs(), reward, terminated, truncated, self._get_info()
 
     def buy_cfg(self, cfg_id):
         # calculate reward
-        reward = -1.0 * self.obs["market_incurring_costs"][cfg_id]
+        reward = -1.0 * self._env_state["market_incurring_costs"][cfg_id]
 
         # buy new configuration; time is not updated
-        self.obs["incurred_costs"][self.buffer_idx] = self.obs[
+        self._env_state["incurred_costs"][self.buffer_idx] = self._env_state[
             "market_incurring_costs"
         ][cfg_id]
 
-        self.obs["recurring_costs"][self.buffer_idx] = self.obs[
+        self._env_state["recurring_costs"][self.buffer_idx] = self._env_state[
             "market_recurring_costs"
         ][cfg_id]
 
-        self.obs["production_rates"][self.buffer_idx] = self.obs[
+        self._env_state["production_rates"][self.buffer_idx] = self._env_state[
             "market_production_rates"
         ][cfg_id]
 
-        self.obs["setup_times"][self.buffer_idx] = self.obs["market_setup_times"][
-            cfg_id
-        ]
+        self._env_state["setup_times"][self.buffer_idx] = self._env_state[
+            "market_setup_times"
+        ][cfg_id]
 
-        self.obs["cfgs_status"][self.buffer_idx] = (
-            1 / self.obs["market_setup_times"][cfg_id]
+        self._env_state["cfgs_status"][self.buffer_idx] = (
+            1 / self._env_state["market_setup_times"][cfg_id]
         )
 
-        self.obs["produced_counts"][self.buffer_idx] = 0
+        self._env_state["produced_counts"][self.buffer_idx] = 0
 
         self.buffer_idx += 1
 
@@ -140,30 +104,91 @@ class MfgEnv(gym.Env):
     def continue_production(self):
         # .astype(int) ensures that only ready machines contribute
         reward = -1.0 * np.sum(
-            self.obs["cfgs_status"].astype(int) * self.obs["recurring_costs"]
+            self._env_state["cfgs_status"].astype(int)
+            * self._env_state["recurring_costs"]
         )
 
         # produce products with ready configurations
-        self.obs["produced_counts"] += (
-            self.obs["cfgs_status"].astype(int) * self.obs["production_rates"]
+        self._env_state["produced_counts"] += (
+            self._env_state["cfgs_status"].astype(int)
+            * self._env_state["production_rates"]
         )
 
         # update cfgs status
         # update only ready or being prepared cfgs
-        updates = np.ceil(self.obs["cfgs_status"])
+        updates = np.ceil(self._env_state["cfgs_status"])
         # add small eps to deal with 0.999999xxx
-        progress = [1 / st + 1e-9 if st != 0 else 0 for st in self.obs["setup_times"]]
-        self.obs["cfgs_status"] = np.clip(
-            self.obs["cfgs_status"] + updates * progress, a_min=0, a_max=1
+        progress = [
+            1 / st + 1e-9 if st != 0 else 0 for st in self._env_state["setup_times"]
+        ]
+        self._env_state["cfgs_status"] = np.clip(
+            self._env_state["cfgs_status"] + updates * progress, a_min=0, a_max=1
         )
 
         # update observation
-        self.obs["demand"] = self.demand - np.sum(
-            self.obs["produced_counts"].astype(int)
+        self._env_state["demand"] = self.demand - np.sum(
+            self._env_state["produced_counts"].astype(int)
         )
-        self.obs["demand_time"] -= 1
+        self._env_state["demand_time"] -= 1
 
         return reward
+
+    def encode_obs(self, obs):
+        return np.concatenate(
+            (
+                [obs["demand"], obs["demand_time"]],
+                obs["incurred_costs"],
+                obs["recurring_costs"],
+                obs["production_rates"],
+                obs["setup_times"],
+                obs["cfgs_status"],
+                obs["produced_counts"],
+                obs["market_incurring_costs"],
+                obs["market_recurring_costs"],
+                obs["market_production_rates"],
+                obs["market_setup_times"],
+            )
+        ).astype(np.float32)
+
+    def decode_obs(self, obs_vec):
+        obs_dict = {}
+        obs_dict["demand"] = obs_vec[0]
+        obs_dict["demand_time"] = obs_vec[1]
+
+        start = 2
+        obs_dict["incurred_costs"] = obs_vec[start : start + self.buffer_size]
+
+        start += self.buffer_size
+        obs_dict["recurring_costs"] = obs_vec[start : start + self.buffer_size]
+
+        start += self.buffer_size
+        obs_dict["production_rates"] = obs_vec[start : start + self.buffer_size]
+
+        start += self.buffer_size
+        obs_dict["setup_times"] = obs_vec[start : start + self.buffer_size]
+
+        start += self.buffer_size
+        obs_dict["cfgs_status"] = obs_vec[start : start + self.buffer_size]
+
+        start += self.buffer_size
+        obs_dict["produced_counts"] = obs_vec[start : start + self.buffer_size]
+
+        start += self.buffer_size
+        obs_dict["market_incurring_costs"] = obs_vec[start : start + self.num_cfgs]
+
+        start += self.num_cfgs
+        obs_dict["market_recurring_costs"] = obs_vec[start : start + self.num_cfgs]
+
+        start += self.num_cfgs
+        obs_dict["market_production_rates"] = obs_vec[start : start + self.num_cfgs]
+
+        start += self.num_cfgs
+        obs_dict["market_setup_times"] = obs_vec[start : start + self.num_cfgs]
+
+        return obs_dict
+
+    def _get_obs(self):
+        return self.encode_obs(self._env_state)
 
     def _get_info(self):
         return {}
--- mfgrl/webapp/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-    "first_column": [1, 2, 3, 4],
-    "second_column": [10, 20, 30, 40]
-})
-
-df
