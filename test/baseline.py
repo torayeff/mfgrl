53e72ba9f20d931f6a896838b815a86a0685cc8a
Agajan Torayev <torayeff@gmail.com>
2023-01-08 01:00:57 +0000

    render and baseline

---
+++ test/baseline.py
@@ -0,0 +1,25 @@
+import gymnasium as gym
+import numpy as np
+import random
+
+np.random.seed(42)
+random.seed(42)
+
+env = gym.make("mfgrl:mfgrl/MfgEnv-v0", render_mode="human")
+obs, info = env.reset(seed=42)
+
+total_reward = 0
+count = 0
+while True:
+
+    if count <= 5:
+        action = np.random.randint(0, env.action_space.n)
+    else:
+        action = 5
+
+    obs, reward, terminated, truncated, info = env.step(action)
+    total_reward += reward
+    if terminated or truncated:
+        break
+
+    count += 1
