cd50bd371209a1ceafadf0afdc3d394bd9ed14e9
Agajan Torayev <torayeff@gmail.com>
2023-01-19 23:19:12 +0000

    simple adjustment to new env

--- test/baseline.py
+++ test/baseline.py
@@ -7,8 +7,8 @@ random.seed(4242)
 
 env_config = {
     "data_file": "data.json",
-    "stochastic": False,
-    "render_mode": "human",
+    "stochastic": True,
+    "render_mode": None,
 }
 env = gym.make("mfgrl:mfgrl/MfgEnv-v0", env_config=env_config)
 obs, info = env.reset(seed=42)
@@ -17,23 +17,17 @@ for k, v in env.decode_obs(obs).items():
     print(k, v)
 
 total_reward = 0
-count = 0
+steps = 0
 max_production_action = np.argmax(env.decode_obs(obs)["market_production_rates"])
 while True:
-    count += 1
-
-    if len(np.where(env.decode_obs(obs)["incurred_costs"] == 0)[0]) == 0:
-        # if the buffer is full continue production
-        action = env.action_space.n
-    else:
-        # buy randomly until the buffer is full
-        action = max_production_action
+    steps += 1
+    action = max_production_action
     obs, reward, terminated, truncated, info = env.step(action)
     total_reward += reward
 
     print("".center(100, "-"))
     print(
-        f"i={count}, a={action}, r={reward}, term={terminated}, info={info}, "
+        f"i={steps}, a={action}, r={reward}, term={terminated}, info={info}, "
         f"tr={total_reward}"
     )
     for k, v in env.decode_obs(obs).items():
